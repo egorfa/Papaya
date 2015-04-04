d9e29c260b581a91753c62d480198da18de53c56
Boris Kozyrev <boris.kozyrev@mail.ru>
2015-04-04 21:20:43 +0300

    added dialogRequestBook

---
+++ app/src/main/java/com/yastart/papaya/dialogs/DialogBookRequest.java
@@ -0,0 +1,55 @@
+package com.yastart.papaya.dialogs;
+
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yastart.papaya.R;
+
+/**
+ * Created by 123 on 04.04.2015.
+ */
+public class DialogBookRequest extends DialogFragment implements View.OnClickListener {
+
+    private Context context;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialog_request, container);
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        context = rootView.getContext();
+
+        final ImageView imgBook = (ImageView) rootView.findViewById(R.id.imgDialog);
+        final TextView txtUser = (TextView) rootView.findViewById(R.id.dialogTxtUser);
+        final TextView txtBook = (TextView) rootView.findViewById(R.id.dialogTxtBook);
+        final TextView txtDescr = (TextView) rootView.findViewById(R.id.dialogTxtDescr);
+        final Button btnDecline = (Button) rootView.findViewById(R.id.dialogDecline);
+        final Button btnAccept = (Button) rootView.findViewById(R.id.dialogAccept);
+
+        btnDecline.setOnClickListener(this);
+        btnAccept.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void OnClick(View view){
+        switch (view.getId()) {
+            case R.id.dialogDecline:
+
+                break;
+            case R.id.dialogAccept:
+
+                break;
+        }
+    }
+}
