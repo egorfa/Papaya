1927f32e3210d2a8875ad8cb991f7e4a92ed0b01
Stetsenko Makar <makazone@gmail.com>
2015-04-04 19:25:52 +0300

    configured junit tests for model classes

--- app/src/main/java/com/yastart/papaya/Model/Book.java
+++ app/src/main/java/com/yastart/papaya/Model/Book.java
@@ -1,7 +1,30 @@
 package com.yastart.papaya.Model;
 
+import java.util.ArrayList;
+
 /**
  * Created by makazone on 04.04.15.
  */
 public class Book {
+
+    private int id;
+    private String city;
+    private User owner;
+
+    private String description;
+    private String title;
+    private String authors;
+
+    private String coverUrl;
+
+    public enum bookCondition {
+        BAD, NORMAL, EXCELLENT;
+    }
+
+    // Getters
+
+    public static ArrayList<Book> getBooksForCity(String city) {
+        ArrayList<Book> result = new ArrayList<Book>();
+        return result;
+    }
 }
