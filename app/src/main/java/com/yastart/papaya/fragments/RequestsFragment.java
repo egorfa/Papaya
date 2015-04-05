50fdada6d1eca0b2bd4979e0a7305ef87e5105f6
Boris Kozyrev <boris.kozyrev@mail.ru>
2015-04-05 12:00:44 +0300

    fixed requests

--- app/src/main/java/com/yastart/papaya/fragments/RequestsFragment.java
+++ app/src/main/java/com/yastart/papaya/fragments/RequestsFragment.java
@@ -36,6 +36,8 @@ public class RequestsFragment extends BaseFragment {
         headings = new ArrayList<String>();
         requests = new ArrayList<Request>();
 
+        final StickyListHeadersListView exlv = (StickyListHeadersListView) view.findViewById(R.id.ex_lv);
+
         User u = User.getCurrentUser();
         Request.getRequestsForUser(u, new GetListHandler<ArrayList<Request>>() {
             @Override
@@ -52,6 +54,9 @@ public class RequestsFragment extends BaseFragment {
                     headings.add("Исходящие");
                     requests.add(data.get(0).get(i));
                 }
+
+                StickyLVAdapter adapter = new StickyLVAdapter(getActivity().getBaseContext(), headings, requests);
+                exlv.setAdapter(adapter);
             }
 
             @Override
@@ -59,11 +64,6 @@ public class RequestsFragment extends BaseFragment {
                 Log.d("REQUEST DEBUG", responseError);
             }
         });
-
-        final StickyListHeadersListView exlv = (StickyListHeadersListView) view.findViewById(R.id.ex_lv);
-        StickyLVAdapter adapter = new StickyLVAdapter(getActivity().getBaseContext(), headings, requests);
-        exlv.setAdapter(adapter);
-
 
         exlv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
