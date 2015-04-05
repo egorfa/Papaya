d97e509de4b38f4edb2a9c9a91e90568fdf963b5
Egor <egorfalkov@mail.ru>
2015-04-05 12:14:12 +0400

    RequestActivity

--- app/src/main/java/com/yastart/papaya/fragments/RequestsFragment.java
+++ app/src/main/java/com/yastart/papaya/fragments/RequestsFragment.java
@@ -1,9 +1,11 @@
 package com.yastart.papaya.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.yastart.papaya.R;
 import com.yastart.papaya.adapters.StickyLVAdapter;
@@ -19,6 +21,7 @@ public class RequestsFragment extends BaseFragment {
 
     ArrayList<String> headings;
     ArrayList<String> requests;
+    final int DIALOG=1;
 
     public static RequestsFragment newInstance() {
         RequestsFragment pageFragment = new RequestsFragment();
@@ -43,11 +46,23 @@ public class RequestsFragment extends BaseFragment {
         requests.add("Oxuenno");
         requests.add("Oxuenno");
         requests.add("Oxuenno");
+        requests.add("Oxuenno");
 
         final StickyListHeadersListView exlv = (StickyListHeadersListView)view.findViewById(R.id.ex_lv);
         StickyLVAdapter adapter = new StickyLVAdapter(getActivity().getBaseContext(), headings, requests);
         exlv.setAdapter(adapter);
 
+
+        exlv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent();
+            }
+        });
+
         return view;
     }
+
+
+
 }
