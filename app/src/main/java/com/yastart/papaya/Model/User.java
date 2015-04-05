9d7cdbcbcc5a089d5a59e4a9be52a257e3bd6634
Stetsenko Makar <makazone@gmail.com>
2015-04-05 13:14:07 +0300

    changes

--- app/src/main/java/com/yastart/papaya/Model/User.java
+++ app/src/main/java/com/yastart/papaya/Model/User.java
@@ -126,18 +126,18 @@ public class User {
      */
     public static User getCurrentUser() {
 //        if (currentUser != null) { return currentUser; }
+//
+//        currentUser = new User();
+//        currentUser.setEmail("egorfalkov1999@gmail.com");
+//        currentUser.setUsername("Егорка");
+//        currentUser.setId("5636318331666432");
+//        currentUser.setCity("Moscow");
 
         currentUser = new User();
-        currentUser.setEmail("egorfalkov1999@gmail.com");
-        currentUser.setUsername("Егорка");
-        currentUser.setId("5636318331666432");
+        currentUser.setEmail("makazone@gmail.com");
+        currentUser.setUsername("Makazone");
+        currentUser.setId("1636319331666438");
         currentUser.setCity("Moscow");
-
-//        currentUser = new User();
-//        currentUser.setEmail("makazone@gmail.com");
-//        currentUser.setUsername("Makazone");
-//        currentUser.setId("1636319331666438");
-//        currentUser.setCity("Moscow");
 
         return currentUser;
     }
